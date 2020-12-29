bac23604dbf1b340fd49a755730879eb4f5de964
York Huang <you@example.com>
2020-12-29 15:01:20 +1100

    refactor

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 In the project directory, you can run:
 
+### `yarn dev`
+
+Launches the test runner in the interactive watch mode.
+
 ### `yarn test`
 
-Launches the test runner in the interactive watch mode.<br />
+Run tests
+
+### `yarn start`
+
+Run the web app
+
+## GitHub Page
+
+https://ywongau.github.io/tetris/
--- src/sfx/audio.js
+++ src/sfx/audio.js
@@ -13,29 +13,24 @@ export const Audio = (AudioContext) => {
       .then((data) => data.arrayBuffer())
       .then((arrayBuffer) => context.decodeAudioData(arrayBuffer));
 
-  const play = (promiseOfAudio) => () =>
-    promiseOfAudio.then((audioBuffer) => {
-      const source = context.createBufferSource();
-      source.buffer = audioBuffer;
-      source.connect(context.destination);
-      source.start();
-    });
-
-  const clear1Audio = decode(clear1Mp3);
-  const clear2Audio = decode(clear2Mp3);
-  const clear3Audio = decode(clear3Mp3);
-  const clear4Audio = decode(clear4Mp3);
-  const lockedAudio = decode(lockedMp3);
-  const spawnAudio = decode(spawnMp3);
-  const gameOverAudio = decode(gameOverMp3);
+  const play = (file) => {
+    const promiseOfAudio = decode(file);
+    return () =>
+      promiseOfAudio.then((audioBuffer) => {
+        const source = context.createBufferSource();
+        source.buffer = audioBuffer;
+        source.connect(context.destination);
+        source.start();
+      });
+  };
 
   return {
-    clear1: play(clear1Audio),
-    clear2: play(clear2Audio),
-    clear3: play(clear3Audio),
-    clear4: play(clear4Audio),
-    locked: play(lockedAudio),
-    spawn: play(spawnAudio),
-    gameOver: play(gameOverAudio)
+    clear1: play(clear1Mp3),
+    clear2: play(clear2Mp3),
+    clear3: play(clear3Mp3),
+    clear4: play(clear4Mp3),
+    locked: play(lockedMp3),
+    spawn: play(spawnMp3),
+    gameOver: play(gameOverMp3)
   };
 };
